0a331fb714ff1fd5865bcbdbfec91b212f538f66
Jacob Oyen <oyenj@central.edu>
2016-02-14 19:50:07 -0600

    v0.0.1-beta

--- index.php
+++ index.php
@@ -9,26 +9,22 @@
   </head>
   <body>
     <header>
+      <a href="/"><img src="images/logo.svg" alt="Big Al Speaks"></a>
       <nav>
-        <a href="index.html">Al Dorenkamp</a>
-        <a href="#">Resume</a>
-        <a href="college-credits.html">College Credits</a>
+        <a href="images/Al-Dorenkamp-Resume.pdf">Resume</a>
+        <a href="">College Credits</a>
         <a href="references.html">References</a>
         <a href="mailto:dorenkampa@gmail.com">Request a quote</a>
       </nav>
     </header>
     <section class="container breathe">
-      <div class="one-third column">
-        <h1>Box 1</h1>
-        <p>Body copy would go here...
+      <div class="four columns">
+        <img src="images/headshot.jpg" alt="placeholder">
       </div>
-      <div class="one-third column">
-        <h1>Box 2</h1>
-        <p>Body copy would go here...
-      </div>
-      <div class="one-third column">
-        <h1>Box 3</h1>
-        <p>Body copy would go here...
+      <div class="eight columns">
+        <h2>A note from Big Al</h2>
+        <p>Education and athletics have been the center of my life and career for nearly 40 years. I have experienced developmental strategies as a student-athlete, teacher and coach, parent, high school principal, college educational methods instructor and athletics director. My goal has been to help solve educational issues in schools and develop an environment that supports a healthy and enjoyable learning experience for teachers, coaches, and students. Educators have some of the toughest jobs in America and need practical and proven strategies to deal with students and parents. As an experienced teacher, coach, administrator and national trainer I promised to teach, motivate, and inspire educators in my on-site school trainings and keynotes speeches.</p>
+        <p class="alignCenter breathe"><a href="mailto:dorenkampa@gmail.com" class="redButton">Request a quote</a></p>
       </div>
       <div class="clearBoth"></div>
     </section>
@@ -37,7 +33,67 @@
         <blockquote>&ldquo;Practical Training Methods<br />in Education and Athletics&rdquo;</blockquote>
       </div>
     </section>
-
+    <section class="container breathe">
+      <div class="two-thirds column borderRight">
+        <h1>Athletic Training/Consulting</h1>
+        <ul>
+          <li>Developing vision, mission and belief systems</li>
+          <li>Program and coach evaluations</li>
+          <li>Sportsmanship and character development</li>
+          <li>Helping coaches deal with adversity and discouragement</li>
+          <li>Organizing and communication networking with a coaching staff</li>
+          <li>Recruitment, selection and induction of personnel</li>
+          <li>Dealing successfully with parents</li>
+          <li>Time management</li>
+          <li>Public relations tips</li>
+        </ul>
+      </div>
+        <div class="one-third column">
+          <h2>My Bio</h2>
+          <ul>
+            <li>Former teacher/coach</li>
+            <li>National trainer in classroom management</li>
+            <li>Former high school principal</li>
+            <li>College athletics director</li>
+            <li>College education methods instructor</li>
+            <li>Student-teacher advisor</li>
+            <li>All-American and Captain of DIII football national champions</li>
+            <li>Banquet, clinic and education speaker</li>
+            <li>B.A., M.A. and education specialist degrees</li>
+          </ul>
+        </div>
+      <div class="clearBoth"></div>
+    </div>
+    </section>
+    <section class="redQuote">
+      <div class="container">
+        <blockquote>Time to Teach certified</blockquote>
+      </div>
+    </section>
+    <section class="container breathe">
+      <div class="one-third column">
+        <img src="images/timeToTeach.jpg" alt="placeholder">
+      </div>
+      <div class="two-thirds column">
+        <h2>Classroom Management Training</h2>
+        <ul>
+          <li>Self-Control Strategies</li>
+          <li>Student-Teacher relationships</li>
+          <li>Teaching rules and procedures</li>
+          <li>Succesfully responding to discipline challenges</li>
+          <li>Classroom ecology and arrangement</li>
+          <li>Takling stressors out of the classroom</li>
+          <li>One or two day training sessions supported by research based materials and manuals</li>
+        </ul>
+      </div>
+      <div class="clearBoth"></div>
+    </div>
+    </section>
+    <footer class="breathe">
+      <p class="alignCenter">Contact me today:<br />
+        <strong>Phone:</strong> 641-780-3700<br />
+        <strong>Email:</strong> dorenkamp@gmail.com</p>
+    </footer>
     <link href="style.css" rel='stylesheet'>
     <link href='//fonts.googleapis.com/css?family=Rajdhani:700' rel='stylesheet' type='text/css'>
     <?php if($_SERVER['HTTP_HOST'] == '192.168.56.111'){ ?>
